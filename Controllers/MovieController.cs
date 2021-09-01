2c420e92304abbef98fc8c95962ded6691ea37a5
vanderL <vnd.vander@gmail.com>
2021-08-31 23:52:20 -0300

    Construindo a API

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+        
+        [HttpPost]
+        public void AddMovie([FromBody] Movie movie)
+        {
+            movies.Add(movie);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Models
+{
+    public class Movie
+    {
+        [Required(ErrorMessage = "O campo título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração máxima é de 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
